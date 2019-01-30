110ed7c907400c99bf7ee1e56515f8fb2fa7ae12
Bilal Maroof <bilal.maroof@avanzainnovations.com>
2019-01-30 20:18:34 +0400

    endpoint changes

--- BAL/sendGetExternal.js
+++ BAL/sendGetExternal.js
@@ -3,15 +3,16 @@ let Config = global.config.eventService;
 const logger = require('../logger');
 
 let internal = (obj, body) => {
-    let url= global.config.eventService.ISC_URL;
+    let url = global.config.eventService.ISC_URL;
     let rpOptions = {
         method: 'POST',
         url,
-        body: Object.assign({
+        body: {
             header: Config.Avanza_ISC,
             template: obj.templateName,
-            endpoint: obj.endpointName
-        }, body),
+            endpoint: obj.endpointName,
+            eventData: body
+        },
         json: true
     };
     logger.info({ fs: 'RequestPromise', func: 'requestPromise' }, "[EPS][RP][SEND]", url, JSON.stringify(rpOptions.body || rpOptions));
@@ -20,7 +21,7 @@ let internal = (obj, body) => {
 
 
 let external = (obj, body) => {
-    let url=`${obj.endpointName.address}${obj.requestURL == '/' ? "" : obj.requestURL}`
+    let url = `${obj.endpointName.address}${obj.requestURL == '/' ? "" : obj.requestURL}`
     let rpOptions = {
         method: 'POST',
         url,
