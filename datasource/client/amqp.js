8bf9bd4063353151412c72fea7be4f21b3bb0540
mzain92 <3287275@gmail.com>
2019-06-17 13:00:17 +0400

    Code after apply new amq lib zain

---
+++ datasource/client/amqp.js
@@ -0,0 +1,39 @@
+const amqplib = require('amqp-connection-manager');
+const crypto = require('crypto');
+var AMQPExistingList = {};
+
+module.exports =  function (connectionURL,rxQueueName,isDurable) {
+    const hash = crypto.createHash('md5').update(connectionURL).digest("hex");
+   
+
+    if (AMQPExistingList[hash] && AMQPExistingList[hash].isConnected() ) {
+        console.log('Returning a MQ instance');
+        return AMQPExistingList[hash];
+    } else {
+        console.log('Creating a MQ instance');
+        try {
+          
+                const conn =  amqplib.connect([connectionURL]);
+                conn.on("close", function () {
+                    console.error("[AMQP] reconnecting");
+                    return setTimeout(createConnection, 7000);
+                });
+                let channelWrapper  =  conn.createChannel({
+                    json: true,
+                    setup: function(channel) {
+                        // `channel` here is a regular amqplib `ConfirmChannel`.
+                        // Note that `this` here is the channelWrapper instance.
+                        return channel.assertQueue(rxQueueName, {durable: isDurable});
+                    }
+                });
+                console.log("channel wrapper "+channelWrapper)
+                AMQPExistingList[hash] = channelWrapper;
+                return channelWrapper;
+            
+        } catch (err) {
+            console.log(err);
+            setTimeout(createConnection, 7000);
+        }
+    }
+    return AMQPExistingList[hash];
+};
--- datasource/queue.js
+++ datasource/queue.js
@@ -1,37 +1,35 @@
 'use strict';
-const amqp = require('amqplib/callback_api');
-const remedy = require('amqplib-auto-recovery');
+
+const amq = require('./client/amqp');
 const crypto = require('../crypto');
+
 function consume(connectionstring, queueName) {
-    remedy(amqp, {
-        onError: (err) => { console.log(err.message) },
-        isErrorUnrecoverable: (err) => false
-    }).connect(connectionstring, function (err, connection) {
-        if (err) {
-            console.log(" [AMQP] ", err.message);
-            return;
-        }
-        console.log(`[*] Connection Aquired: ${connectionstring}`);
-        connection.createChannel(function (err, channel) {
-            channel.assertQueue(queueName, {
-                durable: false
-            });
-            channel.prefetch(1);
-            console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queueName);
-            channel.consume(queueName, function (msg) {
-                console.log(`[*] Waiting for messages ${queueName}`);
-                let event = undefined;
-                try { event = JSON.parse(msg.content.toString()) }
-                catch (ex) {console.log(`message discarded due bad JSON from Queue  ${queueName}`) }
-                if (event) {
-                    console.log(`----- Got EVENT -------\n${JSON.stringify(event, null, 2)}`)
-                    process.send(event)
-                };
-                channel.ack(msg);
-            }, { noAck: false });
-        });
+   var  channelwrapper= amq(connectionstring,queueName,false)
+  
+   channelwrapper.addSetup(setupFn, err => {
+        if(err) {console.log("Error adding setup function");}
     });
+        console.log(`[*] Connection Aquired: ${connectionstring}`);
+
+           //function
+           function setupFn(channel, done) {
+                    channel.consume(queueName, function (msg) {
+                        console.log(`[*] Waiting for messages ${queueName}`);
+                        let event = undefined;
+                        try { event = JSON.parse(msg.content.toString()) }
+                        catch (ex) {console.log(`message discarded due bad JSON from Queue  ${queueName}`) }
+                        if (event) {
+                            console.log(`----- Got EVENT -------\n${JSON.stringify(event, null, 2)}`)
+                            process.send(event)
+                        };
+                        channel.ack(msg);
+                    }, done);
+                }
+
+                
+        
 }
+
 
 
 process.once('message', (arg) => {
